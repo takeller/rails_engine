9e76a4b0b83bf1e9fdcb991b377552b4bbdf4ac9
Taylor Keller <takeller17@gmail.com>
2020-07-13 16:42:12 -0600

    Adds merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
+      resources :merchants do
+        resources :items
+      end
     end
   end
 end
