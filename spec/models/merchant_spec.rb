b4a535142697f3db58c0df7be12231aee1e38ef2
Taylor Keller <takeller17@gmail.com>
2020-07-14 16:22:55 -0600

    Adds specs for testing find endpoints

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,15 @@ describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  describe 'Class methods' do
+    it '.find_by_attribute' do
+      create_list(:merchant, 3)
+      create(:merchant, name: 'Sakka')
+
+      merchant = Merchant.find_by_attribute({name: 'Sakka'})
+
+      expect(merchant.first.name).to eq('Sakka')
+    end
+  end
 end
--- spec/requests/api/v1/merchant_find_spec.rb
+++ spec/requests/api/v1/merchant_find_spec.rb
@@ -1,25 +1,59 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  it 'Can find a merchant based on a single criteria' do
+  it 'Can find a merchant based on name' do
     create_list(:merchant, 10)
-    create(:merchant, name: 'Sakka')
+    create(:merchant, name: 'Aang')
 
-    get '/api/v1/merchants/find?name=Sakka'
+    get '/api/v1/merchants/find?name=Aang'
 
     expect(response).to be_successful
 
     merchant = JSON.parse(response.body)
 
     expect(merchant['data'].count).to eq(1)
-    expect(merchant['data']['attributes']['name']).to eq('Sakka')
+    expect(merchant['data'].first['attributes']['name']).to eq('Aang')
   end
 
   it 'Search parameter is case insensitive' do
+    create_list(:merchant, 10)
+    create(:merchant, name: 'Aang')
 
+    get '/api/v1/merchants/find?name=AAnG'
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant['data'].count).to eq(1)
+    expect(merchant['data'].first['attributes']['name']).to eq('Aang')
+  end
+
+  it 'Search parameter can be a partial match' do
+    create_list(:merchant, 10)
+    create(:merchant, name: 'Aang')
+
+    get '/api/v1/merchants/find?name=ng'
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant['data'].count).to eq(1)
+    expect(merchant['data'].first['attributes']['name']).to eq('Aang')
   end
 
   it 'Can find a merchant based on multiple criteria' do
+    create_list(:merchant, 10)
+    aang = create(:merchant, name: 'Aang')
 
+    get "/api/v1/merchants/find?name=ng&created_at=#{aang.created_at}"
+    
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant['data'].count).to eq(1)
+    expect(merchant['data'].first['attributes']['name']).to eq('Aang')
   end
 end
