f807d8efcc0279b64af433bc8ef0938f669c6461
Taylor Keller <takeller17@gmail.com>
2020-07-13 18:02:21 -0600

    Adds merchant items route and merchant item logic to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:merchant_id]
+      merchant = Merchant.find(params[:merchant_id])
+      items = merchant.items
+    else
+      items = Item.all
+    end
     render json: ItemSerializer.new(items)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items
       resources :merchants do
-        resources :items
+        resources :items, only: [:index]
       end
     end
   end
