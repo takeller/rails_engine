ce5a7176943128d9519f7d9dfbcba7ba65abc3c6
Taylor Keller <takeller17@gmail.com>
2020-07-14 17:48:58 -0600

    Adds most_revenue class method to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,16 @@ class Merchant < ApplicationRecord
 
     where(search_params).first
   end
+
+  def self.most_revenue(quantity)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .where(transactions: { result: "success" })
+      .group(:id)
+      .order("revenue DESC")
+      .limit(quantity)
+  end
 end
+
+
+# Merchant.select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(invoices: [:invoice_items, :transactions]).where(transactions: { result: "success" }).group(:id).order("revenue DESC").limit(10).to_sql
