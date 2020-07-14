e144f003416cf05c9d4797bd8aac53c2a3e1413a
Taylor Keller <takeller17@gmail.com>
2020-07-13 18:13:48 -0600

    Fix typo in route controller name

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items do
-        get 'merchant', to: 'merchant#show'
+        get 'merchant', to: 'merchants#show'
       end
       resources :merchants do
         resources :items, only: [:index]
