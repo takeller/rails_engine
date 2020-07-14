fe5dabf6afb153091349eaaed6e429cc35d10603
Taylor Keller <takeller17@gmail.com>
2020-07-14 16:22:00 -0600

    Adds find_by_attribute class method, and extends class with Searchable module

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,12 @@
 class Merchant < ApplicationRecord
+  extend Searchable
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
+
+  def self.find_by_attribute(search_params)
+    search_params = format_params(search_params)
+
+    where(search_params)
+  end
 end
